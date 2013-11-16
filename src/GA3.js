ed3995dd7cb2dcfceb97d439cb5987e43c40823f
Brandon Autrey <b.autrey@ieee.org>
2013-11-16 14:51:56 -0500

    html modified, models not showing

--- src/GA3.js
+++ src/GA3.js
@@ -1,22 +1,35 @@
+/**
+ * Main javascript file
+ */
+
 "use strict";
 //This function gets called when reading a JSON file. It stores the current xml information.
 
-var newModelFlag = true;
-var dollyRequired = 0;
-var rotateFlag = true;
-function toggleRotateFlag() {
-    rotateFlag = !rotateFlag;
+var floodFlag = true;
+var sunAngle = 32; //degrees
+
+function toggleFloodFlag() {
+    floodFlag = !floodFlag;
+    document.getElementById("myCanvas1").focus();
+    document.getElementById("flood").blur();
+}
+
+function changeSun(value) {
+    sunAngle = value;
+    sunNum.innerHTML = value;
+    document.getElementById("myCanvas1").focus();
+    document.getElementById("sun").blur();
+}
+
+function addMessage(message) {
+    console.log(message);
 }
 
 function main() {
     // ... global variables ...
-    var gl, model, camera, program;
-    var canvas = null;
-    var messageField = null;
-
-    function addMessage(message) {
-        console.log(message);
-    }
+    var gl, canvas, model, camera, program;
+    var projMatrix, viewMatrix;
+    var fov = 32;
 
     canvas = document.getElementById("myCanvas1");
     //addMessage(((canvas)?"Canvas acquired":"Error: Can not acquire canvas"));
@@ -24,50 +37,159 @@ function main() {
     canvas.height = window.innerHeight;
     gl = getWebGLContext(canvas, false);//Disable debugging
 
-    var angle = 0;
+    var intervalL = null, intervalR = null, intervalU = null, intervalDo = null;
+    var intervalW = null, intervalA = null, intervalS = null, intervalD = null;
+    var intervalF = null;
+
     program = createShaderProgram(gl);
     gl.clearColor(0, 0, 0, 1);
     gl.enable(gl.DEPTH_TEST);
+    newModel();
     draw();
-    return 1;
-    function draw() {
-        if (newModelFlag)newModel();
-        if (dollyRequired) {
-            camera.dolly(0.05 * dollyRequired);
-            dollyRequired = 0;
+/*
+    // Set up key listeners to move through the world.
+    window.onkeydown =
+        function (e) {
+            if (e.keyCode == 37 && intervalL == null) {
+                intervalL = setInterval(function () {
+                    // User pressed left arrow.  Pan (Look left)
+                    viewMatrix = camera.panLeft();
+                }, 30);
+            }
+            if (e.keyCode == 39 && intervalR == null) {
+                intervalR = setInterval(function () {
+                    // User pressed right arrow.  Pan (Look right)
+                    viewMatrix = camera.panRight();
+                }, 30);
+            }
+            if (e.keyCode == 38 && intervalU == null) {
+                intervalU = setInterval(function () {
+                    // User pressed up arrow.  Tilt (Look up)
+                    viewMatrix = camera.tiltUp();
+                }, 30);
+            }
+            if (e.keyCode == 40 && intervalDo == null) {
+                intervalDo = setInterval(function () {
+                    // User pressed down arrow.  Tilt (Look down)
+                    viewMatrix = camera.tiltDown();
+                }, 30);
+            }
+            if (e.keyCode == 65 && intervalA == null) {
+                intervalA = setInterval(function () {
+                    // User pressed 'a' key. Truck (Step left)
+                    viewMatrix = camera.truckLeft();
+                    // The light source is always at the eye.
+                    model.lightPosition = [camera.getEye()[0], camera.getEye()[1], camera.getEye()[2]];
+                }, 30);
+            }
+            if (e.keyCode == 68 && intervalD == null) {
+                intervalD = setInterval(function () {
+                    // User pressed 'd' key. Truck (Step right)
+                    viewMatrix = camera.truckRight();
+                    // The light source is always at the eye.
+                    model.lightPosition = [camera.getEye()[0], camera.getEye()[1], camera.getEye()[2]];
+                }, 30);
+            }
+            if (e.keyCode == 82 && intervalR == null) {
+                intervalR = setInterval(function () {
+                    // User pressed 'r' key. Dolly (Step in)
+                    viewMatrix = camera.dollyToward();
+                }, 30);
+            }
+            if (e.keyCode == 70 && intervalF == null) {
+                intervalF = setInterval(function () {
+                    // User pressed 'f' key. Dolly (Step back)
+                    viewMatrix = camera.dollyBack();
+                }, 30);
+            }
+            if (e.keyCode == 87 && intervalW == null) {
+                intervalW = setInterval(function () {
+                    // User pressed 'w' key. Pedestal (move up)
+                    viewMatrix = camera.pedestalUp();
+                    model.lightPosition = [camera.getEye()[0], camera.getEye()[1], camera.getEye()[2]];
+                }, 30);
+            }
+            if (e.keyCode == 83 && intervalS == null) {
+                intervalS = setInterval(function () {
+                    // User pressed 's' key. Pedestal (move down)
+                    viewMatrix = camera.pedestalDown();
+                    model.lightPosition = [camera.getEye()[0], camera.getEye()[1], camera.getEye()[2]];
+                }, 30);
+            }
+
+            draw();
+        };
+
+    // Clear previous keydown interval.
+    window.onkeyup = function (e) {
+        if (e.keyCode == 37) {
+            clearInterval(intervalL);
+            intervalL = null;
         }
+        if (e.keyCode == 39) {
+            clearInterval(intervalR);
+            intervalR = null;
+        }
+        if (e.keyCode == 38) {
+            clearInterval(intervalU);
+            intervalU = null;
+        }
+        if (e.keyCode == 40) {
+            clearInterval(intervalDo);
+            intervalDo = null;
+        }
+        if (e.keyCode == 65) {
+            clearInterval(intervalA);
+            intervalA = null;
+        }
+        if (e.keyCode == 68) {
+            clearInterval(intervalD);
+            intervalD = null;
+        }
+        if (e.keyCode == 82) {
+            clearInterval(intervalR);
+            intervalR = null;
+        }
+        if (e.keyCode == 70) {
+            clearInterval(intervalF);
+            intervalF = null;
+        }
+        if (e.keyCode == 87) {
+            clearInterval(intervalW);
+            intervalW = null;
+        }
+        if (e.keyCode == 83) {
+            clearInterval(intervalS);
+            intervalS = null;
+        }
+
+    };
+*/
+    function draw() {
         gl.useProgram(program);
 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        var projMatrix = camera.getProjMatrix();
         gl.uniformMatrix4fv(program.uniformLocations["projT"], false, projMatrix.elements);
-        var viewMatrix = camera.getRotatedViewMatrix(angle);
         gl.uniformMatrix4fv(program.uniformLocations["viewT"], false, viewMatrix.elements);
-
         model.draw();
-        if (rotateFlag) {
-            angle++;
-            if (angle > 360) angle -= 360;
-        }
 
         gl.useProgram(null);
 
         window.requestAnimationFrame(draw);
     }
 
-    function newModel(path) {
-        function getCurrentModelPath() {
-            return document.getElementById("modelList").value;
-            //return pathname;
-        }
-
+    /**
+     * loads in new model, will specify position here
+     */
+    function newModel() {
         if (model) model.delete();
-        if (!path) path = getCurrentModelPath();
-        console.log(path);
+        var path = "House";
+        //console.log(path);
         model = new JsonRenderable(gl, program, "../model/" + path + "/models/", "model.json");
         if (!model)alert("No model could be read");
-        else newModelFlag = false;
         var bounds = model.getBounds();
         camera = new Camera(gl, bounds, [0, 1, 0]);
+        viewMatrix = camera.getViewMatrix();
+        projMatrix = camera.getProjMatrix(fov);
     }
 }
--- src/camera.js
+++ src/camera.js
@@ -2,40 +2,232 @@ function Camera(gl, d, modelUp) // Compute a camera from model's bounding box di
 {
     var center = [(d.min[0] + d.max[0]) / 2, (d.min[1] + d.max[1]) / 2, (d.min[2] + d.max[2]) / 2];
     var diagonal = Math.sqrt(Math.pow((d.max[0] - d.min[0]), 2) + Math.pow((d.max[1] - d.min[1]), 2) + Math.pow((d.max[2] - d.min[2]), 2));
-    //console.log(center+" "+diagonal);
 
-    var name = "auto";
-    var at = [center[0], center[1], center[2]];
-
-    var eye = [at[0], at[1], at[2] + diagonal * 1.5];
+    var at = center;
+    var eye = [center[0], center[1] + diagonal * 0.5, center[2] + diagonal * 1.5];
     var up = [modelUp[0], modelUp[1], modelUp[2]];
-    var near = diagonal * 0.1;
-    var far = diagonal * 3;
-    var FOV = 32;
-    this.dolly = function (delta) {
-        var m = this.getViewMatrix();
-        var W = [m.elements[2], m.elements[6], m.elements[10]];
-        var deltaEye = [delta * diagonal * W[0], delta * diagonal * W[1], -delta * diagonal * W[2]];
-        //console.log(deltaEye);
-        eye[0] += deltaEye[0];
-        eye[1] += deltaEye[1];
-        eye[2] += deltaEye[2];
-        //at[0] += deltaEye[0];at[1] += deltaEye[1];at[2] += deltaEye[2];
-        //console.log(eye);
-    };
-    this.getRotatedCameraPosition = function (angle) {
-        var m = new Matrix4().setTranslate(center[0], center[1], center[2]).rotate(angle, up[0], up[1], up[2]).translate(-center[0], -center[1], -center[2]);
-        var e = m.multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1])).elements;
-        return [e[0], e[1], e[2]];
-    };
+    var fov = 32, near = 0.1, far = 10;
+    var tiltAngle = 0;
+
+
     this.getViewMatrix = function (e) {
         if (e == undefined) e = eye;
         return new Matrix4().setLookAt(e[0], e[1], e[2], at[0], at[1], at[2], up[0], up[1], up[2]);
     };
-    this.getRotatedViewMatrix = function (angle) {
-        return this.getViewMatrix(this.getRotatedCameraPosition(angle));
+
+    this.getEye = function () {
+        return eye;
     };
-    this.getProjMatrix = function () {
-        return new Matrix4().setPerspective(FOV, gl.canvas.width / gl.canvas.height, near, far);
+    this.getAt = function () {
+        return at;
+    };
+    this.getProjMatrix = function (FOV) {
+        fov = FOV;
+        //addMessage("FOV: " + fov + ", near: " + (near / diagonal).toFixed(2) + ", far: " + (far / diagonal).toFixed(2));
+        return new Matrix4().setPerspective(fov, gl.canvas.width / gl.canvas.height, near, far);
+    };
+    // User pressed left arrow. Pan (Look left)
+    this.panLeft = function () {
+        var m = this.getViewMatrix();
+        // Get the camera's local y-axis (V-axis).
+        var v = [m.elements[1], m.elements[5], m.elements[9]];
+        // Rotate matrix by one degree.
+        var rotM = new Matrix4().setTranslate(eye[0], eye[1], eye[2]).rotate(1.5, v[0], v[1], v[2]).translate(-eye[0], -eye[1], -eye[2]);
+        // Multiply by old at vector.
+        var newAt = rotM.multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        // Set camera's new view matrix.
+        return this.getViewMatrix();
+    };
+    // User pressed right arrow. Pan (Look right)
+    this.panRight = function () {
+        var m = this.getViewMatrix();
+        // Get the camera's local y-axis (-axis).
+        var v = [m.elements[1], m.elements[5], m.elements[9]];
+        // Rotate matrix by one degree.
+        var rotM = new Matrix4().setTranslate(eye[0], eye[1], eye[2]).rotate(-1.5, v[0], v[1], v[2]).translate(-eye[0], -eye[1], -eye[2]);
+        // Multiply by old at vector.
+        var newAt = rotM.multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        // Set camera's new view matrix.
+        return this.getViewMatrix();
+    };
+    // User pressed up arrow. Tilt (Look up)
+    this.tiltUp = function () {
+        // Do not allow unlimited tilting.
+        if (tiltAngle < 90) {
+            tiltAngle++;
+
+            var m = this.getViewMatrix();
+            // Get the camera's local x-axis (U-axis).
+            var u = [m.elements[0], m.elements[4], m.elements[8]];
+            // Rotate matrix by one degree.
+            var rotM = new Matrix4().setTranslate(eye[0], eye[1], eye[2]).rotate(1, u[0], u[1], u[2]).translate(-eye[0], -eye[1], -eye[2]);
+            // Multiply by old at vector.
+            var newAt = rotM.multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+            at[0] = newAt.elements[0];
+            at[1] = newAt.elements[1];
+            at[2] = newAt.elements[2];
+        }
+        // Set camera's new view matrix.
+        return this.getViewMatrix();
+    };
+    // User pressed down arrow. Tilt (Look down)
+    this.tiltDown = function () {
+        // Do not allow unlimited tilting.
+        if (tiltAngle > -90) {
+            tiltAngle--;
+
+            var m = this.getViewMatrix();
+            // Get the camera's local x-axis (U-axis).
+            var u = [m.elements[0], m.elements[4], m.elements[8]];
+            // Rotate matrix by one degree.
+            var rotM = new Matrix4().setTranslate(eye[0], eye[1], eye[2]).rotate(-1, u[0], u[1], u[2]).translate(-eye[0], -eye[1], -eye[2]);
+            // Multiply by old at vector.
+            var newAt = rotM.multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+            at[0] = newAt.elements[0];
+            at[1] = newAt.elements[1];
+            at[2] = newAt.elements[2];
+        }
+        // Set camera's new view matrix.
+        return this.getViewMatrix();
+    };
+    // User pressed 'a' key. Truck (Step left)
+    this.truckLeft = function () {
+        var delta = diagonal * 0.05;
+        var m = this.getViewMatrix();
+        // Get the camera's local x-axis (U-axis).
+        var u = [m.elements[0], m.elements[4], m.elements[8]];
+
+        var newEye = new Matrix4()
+            .setTranslate(-u[0] * delta, -u[1] * delta, -u[2] * delta)
+            .multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1]));
+
+        var newAt = new Matrix4()
+            .setTranslate(-u[0] * delta, -u[1] * delta, -u[2] * delta)
+            .multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+
+        eye[0] = newEye.elements[0];
+        eye[1] = newEye.elements[1];
+        eye[2] = newEye.elements[2];
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        return this.getViewMatrix();
+    };
+    // User pressed 'd' key. Truck (Step right)
+    this.truckRight = function () {
+        var delta = diagonal * 0.05;
+        var m = this.getViewMatrix();
+        // Get the camera's local x-axis (U-axis).
+        var u = [m.elements[0], m.elements[4], m.elements[8]];
+
+        var newEye = new Matrix4()
+            .setTranslate(u[0] * delta, u[1] * delta, u[2] * delta)
+            .multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1]));
+
+        var newAt = new Matrix4()
+            .setTranslate(u[0] * delta, u[1] * delta, u[2] * delta)
+            .multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+
+        eye[0] = newEye.elements[0];
+        eye[1] = newEye.elements[1];
+        eye[2] = newEye.elements[2];
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        return this.getViewMatrix();
+    };
+
+    // User pressed 'r' key. Dolly (Step in)
+    this.dollyToward = function () {
+        var delta = .02;
+        var w = [eye[0] - at[0], eye[1] - at[1], eye[2] - at[2]];
+        var newEye = new Matrix4()
+            .setTranslate(-w[0] * delta, -w[1] * delta, -w[2] * delta)
+            .multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1]));
+        var newAt = new Matrix4()
+            .setTranslate(-w[0] * delta, -w[1] * delta, -w[2] * delta)
+            .multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+
+        eye[0] = newEye.elements[0];
+        eye[1] = newEye.elements[1];
+        eye[2] = newEye.elements[2];
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        return this.getViewMatrix();
+    };
+
+    // User pressed 'f' key. Dolly (Step back)
+    this.dollyBack = function () {
+        var delta = .02;
+        var w = [eye[0] - at[0], eye[1] - at[1], eye[2] - at[2]];
+        var newEye = new Matrix4()
+            .setTranslate(w[0] * delta, w[1] * delta, w[2] * delta)
+            .multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1]));
+        var newAt = new Matrix4()
+            .setTranslate(w[0] * delta, w[1] * delta, w[2] * delta)
+            .multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+
+        eye[0] = newEye.elements[0];
+        eye[1] = newEye.elements[1];
+        eye[2] = newEye.elements[2];
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        return this.getViewMatrix();
+    };
+
+    // User pressed 'w' key. Pedestal (Move up)
+    this.pedestalUp = function () {
+        var delta = diagonal * 0.05;
+
+        var newEye = new Matrix4()
+            .setTranslate(up[0] * delta, up[1] * delta, up[2] * delta)
+            .multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1]));
+        var newAt = new Matrix4()
+            .setTranslate(up[0] * delta, up[1] * delta, up[2] * delta)
+            .multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+
+        eye[0] = newEye.elements[0];
+        eye[1] = newEye.elements[1];
+        eye[2] = newEye.elements[2];
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        return this.getViewMatrix();
+    };
+
+    // User pressed 's' key. Pedestal (Move down)
+    this.pedestalDown = function () {
+        var delta = diagonal * 0.05;
+
+        var newEye = new Matrix4()
+            .setTranslate(-up[0] * delta, -up[1] * delta, -up[2] * delta)
+            .multiplyVector4(new Vector4([eye[0], eye[1], eye[2], 1]));
+        var newAt = new Matrix4()
+            .setTranslate(-up[0] * delta, -up[1] * delta, -up[2] * delta)
+            .multiplyVector4(new Vector4([at[0], at[1], at[2], 1]));
+
+        eye[0] = newEye.elements[0];
+        eye[1] = newEye.elements[1];
+        eye[2] = newEye.elements[2];
+        at[0] = newAt.elements[0];
+        at[1] = newAt.elements[1];
+        at[2] = newAt.elements[2];
+
+        return this.getViewMatrix();
     };
 }
