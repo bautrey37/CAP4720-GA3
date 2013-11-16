d341040744f0c451e8f9a2346b32a63a0b992eea
Brandon Autrey <b.autrey@ieee.org>
2013-11-16 13:54:14 -0500

    working base project

---
+++ README.md
@@ -0,0 +1,13 @@
+COP4720-GA2
+===========
+
+Computer Graphics Group Assignment Number 2 of Fall 2013 at UCF.  
+
+Team Members
+====
+
+ - Brandon Autrey
+ - Juan Cervera
+ - Mike Funchess
+ - Cole Garner
+ - Mike Zellers
--- src/GA3.js
+++ src/GA3.js
@@ -2,59 +2,72 @@
 //This function gets called when reading a JSON file. It stores the current xml information.
 
 var newModelFlag = true;
-var dollyRequired=0;
-var rotateFlag =true;
-function toggleRotateFlag(){rotateFlag = !rotateFlag;}
+var dollyRequired = 0;
+var rotateFlag = true;
+function toggleRotateFlag() {
+    rotateFlag = !rotateFlag;
+}
 
-function main(){
-	// ... global variables ...
-	var gl,model,camera,program;
-	var canvas = null;
-	var messageField = null;
-	function addMessage(message){
-		console.log(message);
-	}
-	canvas = document.getElementById("myCanvas1");
-	//addMessage(((canvas)?"Canvas acquired":"Error: Can not acquire canvas"));
-	gl = getWebGLContext(canvas,false);//Disable debugging
-		
-	var angle=0;
-	program=createShaderProgram(gl);
-	gl.clearColor(0,0,0,1);
-	gl.enable(gl.DEPTH_TEST);
-	draw();
-	return 1;
-	function draw(){
-		if (newModelFlag)newModel();
-		if (dollyRequired){camera.dolly(0.05*dollyRequired);dollyRequired=0;}
-		gl.useProgram(program);
+function main() {
+    // ... global variables ...
+    var gl, model, camera, program;
+    var canvas = null;
+    var messageField = null;
 
-		gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-		var projMatrix = camera.getProjMatrix();
-		gl.uniformMatrix4fv(program.uniformLocations["projT"], false, projMatrix.elements);
-		var viewMatrix = camera.getRotatedViewMatrix(angle);
-		gl.uniformMatrix4fv(program.uniformLocations["viewT"], false, viewMatrix.elements);
+    function addMessage(message) {
+        console.log(message);
+    }
 
-		model.draw();
-		if (rotateFlag){angle++; if (angle > 360) angle -= 360;}
+    canvas = document.getElementById("myCanvas1");
+    //addMessage(((canvas)?"Canvas acquired":"Error: Can not acquire canvas"));
+    canvas.width = window.innerWidth - 249; //subtracts the toolbar width
+    canvas.height = window.innerHeight;
+    gl = getWebGLContext(canvas, false);//Disable debugging
 
-		gl.useProgram(null);
+    var angle = 0;
+    program = createShaderProgram(gl);
+    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
+    draw();
+    return 1;
+    function draw() {
+        if (newModelFlag)newModel();
+        if (dollyRequired) {
+            camera.dolly(0.05 * dollyRequired);
+            dollyRequired = 0;
+        }
+        gl.useProgram(program);
 
-		window.requestAnimationFrame(draw);
-	}
-	function newModel(path)
-	{
-		function getCurrentModelPath(){
-			return document.getElementById("modelList").value;
-			//return pathname;
-		}
-		if (model) model.delete();
-		if (!path) path = getCurrentModelPath();
-		console.log(path);
-		model=new JsonRenderable(gl,program,"./model/"+path+"/models/","model.json");
-		if (!model)alert("No model could be read");
-		else newModelFlag = false;
-		var bounds = model.getBounds();
-		camera = new Camera(gl,bounds,[0,1,0]);
-	}
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        var projMatrix = camera.getProjMatrix();
+        gl.uniformMatrix4fv(program.uniformLocations["projT"], false, projMatrix.elements);
+        var viewMatrix = camera.getRotatedViewMatrix(angle);
+        gl.uniformMatrix4fv(program.uniformLocations["viewT"], false, viewMatrix.elements);
+
+        model.draw();
+        if (rotateFlag) {
+            angle++;
+            if (angle > 360) angle -= 360;
+        }
+
+        gl.useProgram(null);
+
+        window.requestAnimationFrame(draw);
+    }
+
+    function newModel(path) {
+        function getCurrentModelPath() {
+            return document.getElementById("modelList").value;
+            //return pathname;
+        }
+
+        if (model) model.delete();
+        if (!path) path = getCurrentModelPath();
+        console.log(path);
+        model = new JsonRenderable(gl, program, "../model/" + path + "/models/", "model.json");
+        if (!model)alert("No model could be read");
+        else newModelFlag = false;
+        var bounds = model.getBounds();
+        camera = new Camera(gl, bounds, [0, 1, 0]);
+    }
 }
